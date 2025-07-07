50cf88fe18f57304f91181c28e4fbbb8c40113f8
vphuoc-04 <vanphuoc240904@gmail.com>
2025-07-07 23:37:07 +0700

    add flash light

---
+++ Assets/Scripts/FlashLight.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+    public GameObject flashlight_ground, inticon, flashlight_player;
+
+    private bool playerInTrigger = false;
+
+    void Start()
+    {
+        flashlight_player.SetActive(false); 
+    }
+
+    void Update()
+    {
+        if (playerInTrigger && Input.GetKeyDown(KeyCode.E))
+        {
+            flashlight_ground.SetActive(false);     
+            inticon.SetActive(false);               
+            flashlight_player.SetActive(true);      
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            playerInTrigger = true;
+            inticon.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            playerInTrigger = false;
+            inticon.SetActive(false);
+        }
+    }
+}
